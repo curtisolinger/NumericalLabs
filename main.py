8c344e1798ec5ed3251ddd7da16326a062d483cc
Curtis Olinger <curtisolinger@gmail.com>
2023-07-11 12:47:38 -0700

    Refactored code and cleaned up algorithm

--- main.py
+++ main.py
@@ -3,27 +3,25 @@ from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-import pandas as pd
-import json
-import plotly
-import plotly.express as px
 from semigroups import (
-    min_value_length_c,
     create_semigroup,
-    calculate_max_factorization_length,
-    calculate_num_of_elements_with_max_length_k,
+    calc_num_of_elements_of_len_k,
     create_invariants,
+    create_factorization_fig,
 )
 
-maximal_length = 50
+# Set the max factorization length to which you wish to examine
+N = 50
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 
-class GeneratorForm(FlaskForm):
+
+class Example2Form(FlaskForm):
     generators = StringField('Generators (comma-separated)', validators=[
         DataRequired()], default="3, 5")
     submit = SubmitField('Submit')
+
 
 class SemigroupForm(FlaskForm):
     generators = StringField('Generators (comma-separated)', validators=[
@@ -38,51 +36,35 @@ def home():
 
 @app.route('/semigroups', methods=['GET', 'POST'])
 def semigroups():
+    example_1 = [3, 5, 6, 8, 9, 10, 11, 12, 13, 14]
     form = SemigroupForm()
+    example2form = Example2Form()
 
     if form.validate_on_submit():
-        generators_input = form.generators.data
-        generators = [int(gen) for gen in generators_input.split(",")]
-        generators.sort()
-        c = min_value_length_c(generators, maximal_length)
-        semigroup = create_semigroup(generators, c)
-        invariant_dict = create_invariants(semigroup, generators)
-        max_factorization_lengths = calculate_max_factorization_length(semigroup, generators, c)
-        length_counts = calculate_num_of_elements_with_max_length_k(max_factorization_lengths)
-        df = pd.DataFrame.from_dict(length_counts, orient='index', columns=['num'])
-        df = df[df.index <= maximal_length]
-        df.sort_index(inplace=True)
-        # Insert a row with index 0 and num value 0
-        df.loc[-1] = [0]
-        df.index = df.index + 1
-        df.sort_index(inplace=True)
-        fig = px.line(df)
-        graphjson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-        return render_template('semigroups.html', graphJSON=graphjson, form=form, invariant_dict=invariant_dict)
+        gen = sorted([int(gen) for gen in form.generators.data.split(",")])
+        semigroup = create_semigroup(gen, N)
+        invariant_dict = create_invariants(semigroup, gen)
+        length_counts = calc_num_of_elements_of_len_k(semigroup, gen, N)
+        graphjson = create_factorization_fig(N, length_counts)
 
-    generators = [2, 3]
-    c = min_value_length_c(generators, maximal_length)
-    semigroup = create_semigroup(generators, c)
-    invariant_dict = create_invariants(semigroup, generators)
-    max_factorization_lengths = calculate_max_factorization_length(semigroup, generators, c)
-    length_counts = calculate_num_of_elements_with_max_length_k(max_factorization_lengths)
-    df = pd.DataFrame.from_dict(length_counts, orient='index', columns=['num'])
-    df = df[df.index <= maximal_length]
-    df.sort_index(inplace=True)
-    # Insert a row with index 0 and num value 0
-    df.loc[-1] = [0]
-    df.index = df.index + 1
-    df.sort_index(inplace=True)
-    fig = px.line(df)
-    graphjson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+        return render_template('semigroups.html',
+                               graphJSON=graphjson,
+                               form=form,
+                               invariant_dict=invariant_dict,
+                               example_1=example_1,
+                               )
 
-    exe0 = [3, 5, 6, 8, 9, 10, 11, 12, 13, 14]
+    gen = [2, 3]
+    semigroup = create_semigroup(gen, N)
+    invariant_dict = create_invariants(semigroup, gen)
+    length_counts = calc_num_of_elements_of_len_k(semigroup, gen, N)
+    graphjson = create_factorization_fig(N, length_counts)
 
     return render_template('semigroups.html',
                            graphJSON=graphjson,
                            form=form,
                            invariant_dict=invariant_dict,
-                           exe0=exe0
+                           example_1=example_1,
                            )
 
 
--- semigroups.py
+++ semigroups.py
@@ -1,5 +1,9 @@
 from itertools import combinations_with_replacement
 from collections import Counter
+import pandas as pd
+import json
+import plotly
+import plotly.express as px
 
 
 class SemigroupElement:
@@ -21,78 +25,54 @@ class SemigroupElement:
         return tuple(factorization)
 
 
+# Function to create a list of objects of class Semigroup where each class
+# object contains the multiset, the length, and a method called number() to
+# calculate the number.
+def create_semigroup(gen, N):
+    # Calculate the multiset length c such that the max num of length
+    # 'n' is less than or equal to min num of length c. So,
+    # for example, if gen = {2, 3} and N = 4, then c = 6 since 3 times
+    # 4 = 12 and 12 is equal to 2 times 6.
+    max_num_of_len_n = gen[-1] * N
+    c = N + 1
+    while max_num_of_len_n > gen[0] * c:
+        c += 1
 
-
-
-
-
-# Returns the smallest value c > k for which the intersection of the set of elements
-# of length k and the set of elements of length c is empty
-def min_value_length_c(generators, maximal_length):
-    k = maximal_length
-    # Assign a value to the largest element in the set of elements of length k
-    max_element_of_length_k = k * generators[-1]
-    # Assign to j the value of k and increment j until the intersection of sets
-    # of length k and j are nonempty
-    j = k + 1
-    while j * generators[0] <= max_element_of_length_k:
-        j += 1
-    return j
-
-
-# Function to create a list of objects of class Semigroup where each class object
-# contains the multiset, the length, and a method called number() to calculate
-# the number.
-def create_semigroup(generators, max_length):
     semigroup = []
-    for k in range(1, max_length):
-        multisets_of_size_k = combinations_with_replacement(generators, k)
-        for multisubset in multisets_of_size_k:
-            semigroup.append(SemigroupElement(multisubset, k))
+    for k in range(1, c):
+        multisets_of_size_k = combinations_with_replacement(gen, k)
+        for multiset in multisets_of_size_k:
+            semigroup.append(SemigroupElement(multiset, k))
+
+    semigroup.sort(key=lambda x: x.number())
     return semigroup
 
 
-# Calculate the maximal decomposition length of each number
-def calculate_max_factorization_length(semigroup, generators, c):
-    # Calculate the maximum factorization length for each element in the semigroup
-    #
-    # The index of this list corresponds to the semigroup element and the element
-    # associated to each index is the max factorization length.
-    #
-    # The variable c is the smallest multiset length/cardinality such
-    # c > max_length and the intersection of the multisets
-    # of length max_length and the multisets of length c is empty.
-    # The size of the list is
-    #
-    # The length of this list will be the largest generator times c - 1 (this element
-    # will be in the semigroup) plus 1 to align the indexes.
-
-    list0 = [0] * (generators[-1] * (c - 1) + 1)
-
+# Calculate the max factorization length of each number and the number of
+# elements of length k
+def calc_num_of_elements_of_len_k(semigroup, gen, N):
+    max_len_list = [0] * (N * gen[-1] + 1)
+    max_num = N * gen[-1]
     for element in semigroup:
-        current_number = element.number()
-        current_length = element.cardinality
-        # Check if the current length is greater than the maximum length seen for the number
-        if current_length > list0[current_number]:
+        current_num = element.number()
+        current_len = element.cardinality
+        # Check if the current length is greater than the maximum length
+        # seen for the number
+        if max_num >= current_num and current_len > max_len_list[current_num]:
             # Update the maximum length for the number
-            list0[current_number] = current_length
-    return list0
+            max_len_list[current_num] = current_len
+    counts_of_len_k = Counter(num for num in max_len_list if num != 0)
+    return counts_of_len_k
 
 
-def calculate_num_of_elements_with_max_length_k(list0):
-    # Count the frequency of the lengths in max_lengths, excluding 0
-    list1 = Counter(num for num in list0 if num != 0)
-    return list1
-
-
-def create_invariants(semigroup, generators):
+def create_invariants(semigroup, gen):
     invariant_dict = {0: [[(0, 0)], 0, 0, 0]}
     # Assign to each of the first 12 numbers in semigroup an empty list
     for i in range(20):
         invariant_dict[semigroup[i].number()] = [[], 0, 0, 0]
     for i in range(20):
         number = semigroup[i].number()
-        invariant_dict[number][0].append(semigroup[i].coefficients(generators))
+        invariant_dict[number][0].append(semigroup[i].coefficients(gen))
 
     for value in invariant_dict.values():
         factorization_lengths = []
@@ -104,16 +84,30 @@ def create_invariants(semigroup, generators):
         if value[2] == 0:
             value[3] = 0
         else:
-            value[3] = round(value[1] / value [2],2)
+            value[3] = round(value[1] / value[2], 2)
 
     invariant_dict = dict(list(invariant_dict.items())[:10])
     return invariant_dict
 
-def example1(generators):
+
+def example1(gen):
     semigroup = []
     for k in range(1, 5):
-        multisets_of_size_k = combinations_with_replacement(generators, k)
+        multisets_of_size_k = combinations_with_replacement(gen, k)
         for multisubset in multisets_of_size_k:
             semigroup.append(SemigroupElement(multisubset, k))
     semigroup = set(semigroup)
     return semigroup
+
+
+def create_factorization_fig(N, length_counts):
+    df = pd.DataFrame.from_dict(length_counts, orient='index', columns=['num'])
+    df = df[df.index <= N]
+    df.sort_index(inplace=True)
+    # Insert a row with index 0 and num value 0
+    df.loc[-1] = [0]
+    df.index = df.index + 1
+    df.sort_index(inplace=True)
+    fig = px.line(df)
+    graphjson = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphjson
