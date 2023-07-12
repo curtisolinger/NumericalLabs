e378dd2f19f3d710400798ae2c4887a6c6d64c37
Curtis Olinger <curtisolinger@gmail.com>
2023-07-12 13:14:57 -0700

    Still trying to keep the form submission data visiable when either form is submitted

--- main.py
+++ main.py
@@ -22,60 +22,44 @@ gen2 = [3, 5]
 
 class GeneratorForm1(FlaskForm):
     generators1 = StringField('Generators1 (comma-separated)', validators=[
-        DataRequired()], default="2, 3")
-    submit1 = SubmitField('Submit1')
+        DataRequired()])
+    submit = SubmitField('Submit')
 
+# generators1 = StringField('Generators1 (comma-separated)', validators=[
+        # DataRequired()], default=', '.join(str(x) for x in gen1))
 
 class GeneratorForm2(FlaskForm):
     generators2 = StringField('Generators2 (comma-separated)', validators=[
-        DataRequired()], default="3, 5")
-    submit2 = SubmitField('Submit2')
+        DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/semigroups/form1', methods=['GET', 'POST'])
-def semigroups_form1():
+@app.route('/semigroups', methods=['GET', 'POST'])
+def semigroups():
     example_0 = [3, 5, 6, 8, 9, 10, 11, 12, 13, 14]
-    form1 = GeneratorForm1(request.form)
+
+    form1 = GeneratorForm1()
     form2 = GeneratorForm2()
 
     global gen1
     global gen2
 
+    print(f'Initial gen1: {gen1}, gen2: {gen2}')
+
     if form1.validate_on_submit():
+        # gen1_str = form1.generators1.data
         gen1 = sorted([int(gen) for gen in form1.generators1.data.split(",")])
-
-    semigroup1 = create_semigroup(gen1, N)
-    semigroup2 = create_semigroup(gen2, N)
-    example_1 = create_example_1(semigroup1)
-    invariant_dict = create_invariants(semigroup2, gen2)
-    length_counts = calc_num_of_elements_of_len_k(semigroup2, gen2, N)
-    graphjson = create_factorization_fig(N, length_counts)
-
-    return render_template('semigroups.html',
-                           example_0=example_0,
-                           form1=form1,
-                           example_1=example_1,
-                           form2=form2,
-                           graphJSON=graphjson,
-                           invariant_dict=invariant_dict,
-                           )
-
-
-@app.route('/semigroups/form2', methods=['GET', 'POST'])
-def semigroups_form2():
-    example_0 = [3, 5, 6, 8, 9, 10, 11, 12, 13, 14]
-    form1 = GeneratorForm1()
-    form2 = GeneratorForm2(request.form)
-
-    global gen1
-    global gen2
+        # form1.generators1.data = ', '.join(str(x) for x in gen1)
+        print(f'gen1 form submission {gen1} and gen2 is {gen2}')
 
     if form2.validate_on_submit():
         gen2 = sorted([int(gen) for gen in form2.generators2.data.split(",")])
+        # form2.generators2.data = ', '.join(str(x) for x in gen2)
+        print(f'gen2 form submission {gen2} and gen1 is {gen1}')
 
     semigroup1 = create_semigroup(gen1, N)
     semigroup2 = create_semigroup(gen2, N)
